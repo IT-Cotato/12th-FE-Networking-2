30ff3c0e6c2cecad159c6ac5945848b0f5eebb35
choyeon <cho1428hee@naver.com>
2026-01-26 20:58:19 +0900

    feat: complete practice code for CSRCard and SSRCard

--- src/components/CSRCard.tsx
+++ src/components/CSRCard.tsx
@@ -10,5 +10,14 @@ interface PostData {
 }
 
 export default function CSRCard() {
-  return <>CSR</>;
+  const [data, setData] = useState<PostData | null>(null);
+
+  useEffect(() => {
+    fetch("/api/data")
+      .then((res) => res.json())
+      .then((json) => setData(json));
+  }, []);
+
+  // CSR에서 아직 데이터가 없으면 스켈레톤 UI를 보여줍니다!
+  return <CardLayout type="CSR" isLoading={!data} {...(data || {})} />;
 }
--- src/components/SSRCard.tsx
+++ src/components/SSRCard.tsx
@@ -1,5 +1,44 @@
 import CardLayout from "@/components/layout/CardLayout";
 
 export default async function SSRCard() {
-  return <>SSR</>;
+  try {
+    /**
+     * [TODO 1] 서버에서 데이터를 직접 가져오세요.
+     * - hint: const res = await fetch("주소", { 옵션 });
+     *
+     * - URL: "http://localhost:3000/api/data"
+     * - 옵션: { cache: "no-store" }
+     */
+    const res = null;
+    // 위 null 부분을 힌트를 이용하여 완성하세요!!!
+    // 완성하면 아래 res의 오류는 사라집니다.
+    if (!res.ok) throw new Error();
+
+    /**
+     * [TODO 2] 응답받은 데이터를 JSON으로 변환하세요.
+     * - hint: await res.json()을 사용하세요.
+     */
+    const data = null; // 이 null부분에 hint로 코드를 완성하세요!!
+
+    /**
+     * [TODO 3] 가져온 데이터를 CardLayout에 넘겨주세요.
+     * - isLoading은 false로 고정합니다.
+     * - data 객체는 spread 연산자(...)를 사용하세요.
+     */
+    return (
+      <CardLayout
+        type="SSR"
+        isLoading={false}
+        /* 여기에 spread 연산자를 사용하여 TODO를 수행하세요 */
+      />
+    );
+  } catch {
+    return (
+      <div className="border-2 border-red-200 rounded-2xl p-6 bg-white shadow-lg flex items-center justify-center min-h-100">
+        <p className="text-red-500 font-medium font-sans">
+          데이터 로드 실패 (서버 에러)
+        </p>
+      </div>
+    );
+  }
 }
