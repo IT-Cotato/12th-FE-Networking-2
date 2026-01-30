8b61b3379769397748a08db60c7373c9b5103c0a
metjyc <metjyc@naver.com>
2026-01-30 20:32:50 +0900

    Update SSRCard.tsx

--- src/components/SSRCard.tsx
+++ src/components/SSRCard.tsx
@@ -9,7 +9,9 @@ export default async function SSRCard() {
      * - URL: "http://localhost:3000/api/data"
      * - 옵션: { cache: "no-store" }
      */
-    const res = null;
+    const res = await fetch("http://localhost:3000/api/data", {
+      cache: "no-store",
+    });
     // 위 null 부분을 힌트를 이용하여 완성하세요!!!
     // 완성하면 아래 res의 오류는 사라집니다.
     if (!res.ok) throw new Error();
@@ -18,7 +20,7 @@ export default async function SSRCard() {
      * [TODO 2] 응답받은 데이터를 JSON으로 변환하세요.
      * - hint: await res.json()을 사용하세요.
      */
-    const data = null; // 이 null부분에 hint로 코드를 완성하세요!!
+    const data = await res.json(); // 이 null부분에 hint로 코드를 완성하세요!!
 
     /**
      * [TODO 3] 가져온 데이터를 CardLayout에 넘겨주세요.
@@ -29,6 +31,7 @@ export default async function SSRCard() {
       <CardLayout
         type="SSR"
         isLoading={false}
+        {...data}
         /* 여기에 spread 연산자를 사용하여 TODO를 수행하세요 */
       />
     );
