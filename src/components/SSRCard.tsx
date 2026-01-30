db04a367d67c136535cc688556924504b8abdf54
idid10 <ejwuud2@naver.com>
2026-01-30 20:28:13 +0900

    SSRCard.tsx 코드 완성

--- src/components/SSRCard.tsx
+++ src/components/SSRCard.tsx
@@ -2,34 +2,15 @@ import CardLayout from "@/components/layout/CardLayout";
 
 export default async function SSRCard() {
   try {
-    /**
-     * [TODO 1] 서버에서 데이터를 직접 가져오세요.
-     * - hint: const res = await fetch("주소", { 옵션 });
-     *
-     * - URL: "http://localhost:3000/api/data"
-     * - 옵션: { cache: "no-store" }
-     */
-    const res = null;
-    // 위 null 부분을 힌트를 이용하여 완성하세요!!!
-    // 완성하면 아래 res의 오류는 사라집니다.
+    const res = await fetch("http://localhost:3000/api/data", { cache: "no-store"});
     if (!res.ok) throw new Error();
 
-    /**
-     * [TODO 2] 응답받은 데이터를 JSON으로 변환하세요.
-     * - hint: await res.json()을 사용하세요.
-     */
-    const data = null; // 이 null부분에 hint로 코드를 완성하세요!!
-
-    /**
-     * [TODO 3] 가져온 데이터를 CardLayout에 넘겨주세요.
-     * - isLoading은 false로 고정합니다.
-     * - data 객체는 spread 연산자(...)를 사용하세요.
-     */
+    const data = await res.json(); 
     return (
       <CardLayout
         type="SSR"
         isLoading={false}
-        /* 여기에 spread 연산자를 사용하여 TODO를 수행하세요 */
+        {...data}
       />
     );
   } catch {
